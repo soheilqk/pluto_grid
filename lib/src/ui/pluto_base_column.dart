d51696c6f9c9b8428df5a2c7c255f6548df08873
soheilqk <soheil.g.k@gmail.com>
2021-03-30 11:09:56 +0430

    checkbox background color

--- lib/src/ui/pluto_base_column.dart
+++ lib/src/ui/pluto_base_column.dart
@@ -7,12 +7,7 @@ class PlutoBaseColumn extends PlutoStatefulWidget {
   final bool first;
   final bool last;
 
-  PlutoBaseColumn({
-    this.stateManager,
-    this.column,
-    this.first,
-    this.last
-  }) : super(key: column.key);
+  PlutoBaseColumn({this.stateManager, this.column, this.first, this.last}) : super(key: column.key);
 
   @override
   _PlutoBaseColumnState createState() => _PlutoBaseColumnState();
@@ -40,29 +35,22 @@ class _PlutoBaseColumnState extends _PlutoBaseColumnStateWithChange {
       column: widget.column,
     );
     if (showColumnFilter) {
-      if(widget.first){
+      if (widget.first) {
         return Container(
             margin: EdgeInsets.symmetric(vertical: 4),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(5), bottomLeft: Radius.circular(5)),
                 color: Colors.grey[100]),
             child: child);
-      }
-
-      else if(widget.last){
+      } else if (widget.last) {
         return Container(
             margin: EdgeInsets.symmetric(vertical: 4),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(5), bottomLeft: Radius.circular(5)),
                 color: Colors.grey[100]),
             child: child);
-      }
-      else if(widget.first == false && widget.last == false) {
-
-        return Container(
-            margin: EdgeInsets.symmetric(vertical: 4),
-            color: Colors.grey[100],
-            child: child);
+      } else if (widget.first == false && widget.last == false) {
+        return Container(margin: EdgeInsets.symmetric(vertical: 4), color: Colors.grey[100], child: child);
       }
     } else {
       return Container();
@@ -76,23 +64,24 @@ class _PlutoBaseColumnState extends _PlutoBaseColumnStateWithChange {
     );
     return Column(
       children: [
-        // if(widget.first)
-        //   Container(
-        //       decoration: BoxDecoration(
-        //           borderRadius: BorderRadius.only(topLeft: Radius.circular(5), bottomLeft: Radius.circular(5)),
-        //           color: Colors.grey[200]),
-        //       child: child),
-        if(widget.last)
+        if (widget.first)
+          Container(
+              // decoration: BoxDecoration(
+              //   borderRadius: const BorderRadius.only(topLeft: Radius.circular(5), bottomLeft: Radius.circular(5)),
+              //   color: Colors.grey[200],
+              // ),
+              child: child),
+        if (widget.last)
           Container(
               decoration: const BoxDecoration(
                   borderRadius: BorderRadius.only(topRight: Radius.circular(5), bottomRight: Radius.circular(5)),
                   color: Colors.amber),
               child: child),
-        if(widget.first == false && widget.last == false) Container(
-            //color: Colors.grey[200],
-            child: child),
+        if (widget.first == false && widget.last == false)
+          Container(
+              //color: Colors.grey[200],
+              child: child),
         showFilter,
-
       ],
     );
   }
