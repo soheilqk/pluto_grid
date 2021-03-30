530e70dfcbd74ae83435cd6d91104b75b4eae1f1
soheilqk <soheil.g.k@gmail.com>
2021-03-30 11:15:19 +0430

    checkbox background color

--- lib/src/ui/columns/pluto_column_title.dart
+++ lib/src/ui/columns/pluto_column_title.dart
@@ -270,14 +270,13 @@ class _BuildColumnWidget extends StatelessWidget {
                       stateManager: stateManager,
                     ),
                   ),
-                if (column.enableRowChecked) const SizedBox(width: 10),
+                //if (column.enableRowChecked) const SizedBox(width: 10),
                 Expanded(
                   child: Container(
-clipBehavior: Clip.antiAlias,
                     decoration: BoxDecoration(
 
                       color:Colors.amber,
-                      borderRadius: column.enableRowChecked?  BorderRadius.only(topRight: const Radius.circular(5), bottomRight: Radius.circular(5)) : null,
+                      borderRadius: column.enableRowChecked? const BorderRadius.only(topRight:  Radius.circular(5), bottomRight: Radius.circular(5)) : null,
                     ),
                     height: PlutoGridSettings.rowHeight,
                     child: Center(
