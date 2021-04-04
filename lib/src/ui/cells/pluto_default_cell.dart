e6f51023edaa189396d83ec87c13446498bb82c8
Soheil <soheil.q.k@gmail.com>
2021-04-04 15:35:24 +0430

    selected event

--- lib/src/ui/cells/pluto_default_cell.dart
+++ lib/src/ui/cells/pluto_default_cell.dart
@@ -290,8 +290,7 @@ class __CheckboxSelectionWidgetState extends __CheckboxSelectionWidgetStateWithC
     }
 
     widget.stateManager.setRowChecked(widget.row, changed);
-    widget.stateManager.toggleSelectingRow(widget.row.sortIdx,notify: true);
-
+    widget.stateManager.toggleSelectingRow(widget.row.sortIdx);
     setState(() {
       checked = changed;
     });
